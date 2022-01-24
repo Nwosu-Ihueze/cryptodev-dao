c973f30ccde3eef86175fe1d9401d844ef2459c0
Nwosu-Ihueze <nwosunneomagmail.com>
2022-01-23 23:35:55 -0500

    first commit

---
+++ constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0xdfBf4539a592Fad38F2100383329B045CA0E59B1";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
